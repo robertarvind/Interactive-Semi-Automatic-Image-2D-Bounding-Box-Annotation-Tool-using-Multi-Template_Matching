22d001a6b51ed6f13cf2a1afa823967a8488aeb3
Robert Arvind <63512064+robertarvind@users.noreply.github.com>
2021-01-02 09:56:20 +0530

    Update SemAutoAnnot.py

--- SemAutoAnnot.py
+++ SemAutoAnnot.py
@@ -3,7 +3,7 @@ Copyright {2021} {Robert Arvind}
    Licensed under the BSD 3-Clause License (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
+       https://opensource.org/licenses/BSD-3-Clause
 """
 
 
